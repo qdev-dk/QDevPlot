4de9e917f993e41e49ca842b7b95aae630974daa
Rasmus B. Christensen <rbcmail@gmail.com>
2023-03-07 13:42:13 +0100

    black formating

--- qdevplot/legacyqcodes.py
+++ qdevplot/legacyqcodes.py
@@ -6,29 +6,35 @@ from matplotlib import pyplot as plt
 
 def legacy_vna_plot(filepath, **kwargs):
     ax, cb = legacy_qcodes_plot(filepath, **kwargs)
-    qp.change_unit_axis(axes=ax, axis='Y', scale=1e9, newlabel='SPEC frequncy[GHz]')
-    qp.change_unit_axis(axes=ax, axis='X', scale=1, newlabel='Power[dBm]')
-    qp.change_unit_axis(axes=cb, axis='Z', scale=1, newlabel='SPEC Magnitude[dBm]')
+    qp.change_unit_axis(axes=ax, axis="Y", scale=1e9, newlabel="SPEC frequncy[GHz]")
+    qp.change_unit_axis(axes=ax, axis="X", scale=1, newlabel="Power[dBm]")
+    qp.change_unit_axis(axes=cb, axis="Z", scale=1, newlabel="SPEC Magnitude[dBm]")
 
     return ax, cb
 
-def plot_legacy_qcodes_linescoop(filepath, ivallist=None, x_label=None, y_label=None,
-                           cb_label=None, invertaxes=False):
-    _, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=False, constrained_layout=True)
-    
+
+def plot_legacy_qcodes_linescoop(
+    filepath, ivallist=None, x_label=None, y_label=None, cb_label=None, invertaxes=False
+):
+    _, (ax1, ax2) = plt.subplots(
+        2, 1, sharex=True, sharey=False, constrained_layout=True
+    )
+
     df, dim = read_legacy_qcodes(filepath, invertaxes=invertaxes)
 
-    ax1, cb = df_to_pcolor(df, dims=dim, ax=ax1, invertaxes=invertaxes) # qp.df_to_scatter(df, ax=ax1)
+    ax1, cb = df_to_pcolor(
+        df, dims=dim, ax=ax1, invertaxes=invertaxes
+    )  # qp.df_to_scatter(df, ax=ax1)
     xmin = df.iloc[:, 0].min()
     xmax = df.iloc[:, 0].max()
-    legend_list=[]
+    legend_list = []
     for ival in ivallist:
         legend_list.append(str(ival))
-        ax1 = qp.add_line_to_plot(ax1,(xmin,xmax), ival[0])
-        ax1 = qp.add_line_to_plot(ax1,(xmin,xmax), ival[1])
+        ax1 = qp.add_line_to_plot(ax1, (xmin, xmax), ival[0])
+        ax1 = qp.add_line_to_plot(ax1, (xmin, xmax), ival[1])
         ax2 = qp.plot_line_scoop_from_df(df, ival, ax=ax2)
     ax2.legend(legend_list)
-    ax1.set_xlabel('')
+    ax1.set_xlabel("")
     return ax1, cb, ax2
 
 
@@ -39,22 +45,26 @@ def legacy_qcodes_plot(filepath, **kwargs):
 
 
 def read_legacy_qcodes(filepath, invertaxes: bool = False, **kwargs):
-    df = pd.read_csv(filepath, delimiter='\t', header=1)
-    df.rename(columns={df.columns[0]: df.columns[0].split('"')[1::2][0]},
-              inplace=True)
+    df = pd.read_csv(filepath, delimiter="\t", header=1)
+    df.rename(columns={df.columns[0]: df.columns[0].split('"')[1::2][0]}, inplace=True)
     datashape = (np.int64(df.iloc[0][0][1:]), np.int64(df.iloc[0][1]))
     df.drop(index=df.index[0], axis=0, inplace=True)
     if invertaxes:
         column_names = [df.columns[1], df.columns[0], df.columns[2]]
         df = df.reindex(columns=column_names)
-    return df.astype('float64'), datashape
+    return df.astype("float64"), datashape
 
 
-def df_to_pcolor(df, dims,
-                 x_label=None, y_label=None, cb_label=None,
-                 ax=None,
-                 invertaxes: bool = False,
-                 **kwargs):
+def df_to_pcolor(
+    df,
+    dims,
+    x_label=None,
+    y_label=None,
+    cb_label=None,
+    ax=None,
+    invertaxes: bool = False,
+    **kwargs
+):
     ax = qp.if_not_ax_make_ax(ax)
     ax = qp.style_jose(ax)
 
@@ -80,10 +90,10 @@ def df_to_pcolor(df, dims,
 def shaped_data_from_df(df, dims, invertaxes: bool = False):
     data = df.to_numpy()
     if invertaxes:
-        X, Y = np.meshgrid(data[:, 0][:dims[1]], data[:, 1][::dims[1]])
+        X, Y = np.meshgrid(data[:, 0][: dims[1]], data[:, 1][:: dims[1]])
         Z = data[:, 2].reshape(dims)
     else:
-        X, Y = np.meshgrid(data[:, 0][::dims[1]], data[:, 1][:dims[1]])
+        X, Y = np.meshgrid(data[:, 0][:: dims[1]], data[:, 1][: dims[1]])
         Z = data[:, 2].reshape(dims).T
 
     return X.astype(np.float64), Y.astype(np.float64), Z.astype(np.float64)
--- qdevplot/plotfunctions.py
+++ qdevplot/plotfunctions.py
@@ -6,43 +6,81 @@ from typing import Optional
 from functools import partial
 from matplotlib.ticker import FuncFormatter
 
-params = {'legend.fontsize': 16,
-          'figure.figsize': (4, 4),
-         'axes.labelsize': 18,
-         'axes.titlesize':18,
-         'xtick.labelsize':16,
-         'ytick.labelsize':16}
+params = {
+    "legend.fontsize": 16,
+    "figure.figsize": (4, 4),
+    "axes.labelsize": 18,
+    "axes.titlesize": 18,
+    "xtick.labelsize": 16,
+    "ytick.labelsize": 16,
+}
 pylab.rcParams.update(params)
 
 
-def plot_scatter_linescoop(x, y, z, xmeas=None, ymeas=None,
-                           ivallist=None, x_label=None, y_label=None,
-                           cb_label=None):
-    _, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=False, constrained_layout=True)
-    
-    df = data_array_to_sorted_cordinate_df(x, y, z, xnew=xmeas, ynew=ymeas,
-                                           x_label=x_label, y_label=y_label,
-                                           z_label=cb_label)
+def plot_scatter_linescoop(
+    x,
+    y,
+    z,
+    xmeas=None,
+    ymeas=None,
+    ivallist=None,
+    x_label=None,
+    y_label=None,
+    cb_label=None,
+):
+    _, (ax1, ax2) = plt.subplots(
+        2, 1, sharex=True, sharey=False, constrained_layout=True
+    )
+
+    df = data_array_to_sorted_cordinate_df(
+        x,
+        y,
+        z,
+        xnew=xmeas,
+        ynew=ymeas,
+        x_label=x_label,
+        y_label=y_label,
+        z_label=cb_label,
+    )
     ax1, cb = df_to_scatter(df, ax=ax1)
     xmin = df.iloc[:, 0].min()
     xmax = df.iloc[:, 0].max()
-    legend_list=[]
+    legend_list = []
     for ival in ivallist:
         legend_list.append(str(ival))
-        ax1 = add_line_to_plot(ax1,(xmin,xmax), ival[0])
-        ax1 = add_line_to_plot(ax1,(xmin,xmax), ival[1])
+        ax1 = add_line_to_plot(ax1, (xmin, xmax), ival[0])
+        ax1 = add_line_to_plot(ax1, (xmin, xmax), ival[1])
         ax2 = plot_line_scoop_from_df(df, ival, ax=ax2)
     ax2.legend(legend_list)
-    ax1.set_xlabel('')
+    ax1.set_xlabel("")
     return ax1, cb, ax2
 
 
-def plot_scatter_linecut(x, y, z, xmeas=None, ymeas=None,
-                         ylist=None, x_label=None, y_label=None,
-                         cb_label=None):
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=False, constrained_layout=True)
+def plot_scatter_linecut(
+    x,
+    y,
+    z,
+    xmeas=None,
+    ymeas=None,
+    ylist=None,
+    x_label=None,
+    y_label=None,
+    cb_label=None,
+):
+    fig, (ax1, ax2) = plt.subplots(
+        2, 1, sharex=True, sharey=False, constrained_layout=True
+    )
 
-    df = data_array_to_sorted_cordinate_df(x, y, z, xnew=xmeas, ynew=ymeas, x_label=x_label, y_label=y_label, z_label=cb_label)
+    df = data_array_to_sorted_cordinate_df(
+        x,
+        y,
+        z,
+        xnew=xmeas,
+        ynew=ymeas,
+        x_label=x_label,
+        y_label=y_label,
+        z_label=cb_label,
+    )
     ax1, cb = df_to_scatter(df, ax=ax1)
     xmin = df.iloc[:, 0].min()
     xmax = df.iloc[:, 0].max()
@@ -50,24 +88,33 @@ def plot_scatter_linecut(x, y, z, xmeas=None, ymeas=None,
         ax1 = add_line_to_plot(ax1, (xmin, xmax), yval)
 
     ax2 = linecuts(df, ylist, ax=ax2)
-    ax1.set_xlabel('')
+    ax1.set_xlabel("")
     return ax1, cb, ax2
 
 
 def add_line_to_plot(ax, x, y):
-    ax.plot(x, (y,y), '--', color='r')
+    ax.plot(x, (y, y), "--", color="r")
     return ax
 
 
-def plot_scatter(x, y, z, xmeas=None, ymeas=None, x_label=None, y_label=None, cb_label=None):
-    df = data_array_to_sorted_cordinate_df(x, y, z, xnew=xmeas, ynew=ymeas,
-                                           x_label=x_label, y_label=y_label, z_label=cb_label)
+def plot_scatter(
+    x, y, z, xmeas=None, ymeas=None, x_label=None, y_label=None, cb_label=None
+):
+    df = data_array_to_sorted_cordinate_df(
+        x,
+        y,
+        z,
+        xnew=xmeas,
+        ynew=ymeas,
+        x_label=x_label,
+        y_label=y_label,
+        z_label=cb_label,
+    )
     ax, cb = df_to_scatter(df)
     return ax, cb
 
 
-def df_to_scatter(df, x_label=None, y_label=None, cb_label=None,
-                  ax=None, **kwargs):
+def df_to_scatter(df, x_label=None, y_label=None, cb_label=None, ax=None, **kwargs):
 
     x = df.iloc[:, 0].values
     y = df.iloc[:, 1].values
@@ -80,13 +127,14 @@ def df_to_scatter(df, x_label=None, y_label=None, cb_label=None,
 
     if x_label is None:
         x_label = col_names[0]
-    if y_label is  None:
+    if y_label is None:
         y_label = col_names[1]
     if cb_label is None:
-        cb_label = col_names[2]        
+        cb_label = col_names[2]
 
-    mappable = ax.scatter(x=x, y=y, c=z, # vmin=0.00, vmax=4.0,
-                          cmap='RdBu_r', **kwargs)
+    mappable = ax.scatter(
+        x=x, y=y, c=z, cmap="RdBu_r", **kwargs  # vmin=0.00, vmax=4.0,
+    )
     cb = ax.figure.colorbar(mappable, ax=ax)
     cb.set_label(cb_label)
     ax.set_ylabel(y_label)
@@ -95,20 +143,37 @@ def df_to_scatter(df, x_label=None, y_label=None, cb_label=None,
     return ax, cb
 
 
-def plot_line_cuts(x, y, z,
-                   xmeas=None, ymeas=None, ylist=None,
-                   x_label=None, y_label=None, cb_label=None,
-                   ax=None):
-    df = data_array_to_sorted_cordinate_df(x, y, z, xnew=xmeas, ynew=ymeas,
-                                           x_label=x_label, y_label=y_label, z_label=cb_label)
+def plot_line_cuts(
+    x,
+    y,
+    z,
+    xmeas=None,
+    ymeas=None,
+    ylist=None,
+    x_label=None,
+    y_label=None,
+    cb_label=None,
+    ax=None,
+):
+    df = data_array_to_sorted_cordinate_df(
+        x,
+        y,
+        z,
+        xnew=xmeas,
+        ynew=ymeas,
+        x_label=x_label,
+        y_label=y_label,
+        z_label=cb_label,
+    )
     ax = linecuts(df, ylist, ax=ax)
     return ax
+
 
 def linecuts(df, ylist, x_label=None, y_label=None, cb_label=None, ax=None):
     x = df.iloc[:, 0].values
     y = df.iloc[:, 1].values
     z = df.iloc[:, 2].values
-    
+
     col_names = list(df.columns)
 
     if x_label is None:
@@ -116,29 +181,45 @@ def linecuts(df, ylist, x_label=None, y_label=None, cb_label=None, ax=None):
     if y_label is None:
         y_label = col_names[1]
     if cb_label is None:
-        cb_label = col_names[2]  
+        cb_label = col_names[2]
     interpN = interpolate.NearestNDInterpolator(list(zip(x, y)), z)
 
     ax = if_not_ax_make_ax(ax)
     for y_val in ylist:
         interp_z = interpN(x, y_val)
-        line, = ax.plot(x, interp_z, linestyle='--', marker='o')
-        line.set_label(f'{y_label}={y_val}')
+        (line,) = ax.plot(x, interp_z, linestyle="--", marker="o")
+        line.set_label(f"{y_label}={y_val}")
 
     ax.legend()
     plt.ylabel(cb_label)
     plt.xlabel(x_label)
     plt.grid()
-    #plt.ylim(0,4.5)
+    # plt.ylim(0,4.5)
     return ax
 
 
-def plot_line_scoop(x, y, z, xmeas=None, ymeas=None,
-                    ival=None, x_label=None, y_label=None,
-                    cb_label=None, ax=None):
-    df = data_array_to_sorted_cordinate_df(x, y, z, xnew=xmeas, ynew=ymeas,
-                                           x_label=x_label, y_label=y_label,
-                                           z_label=cb_label)
+def plot_line_scoop(
+    x,
+    y,
+    z,
+    xmeas=None,
+    ymeas=None,
+    ival=None,
+    x_label=None,
+    y_label=None,
+    cb_label=None,
+    ax=None,
+):
+    df = data_array_to_sorted_cordinate_df(
+        x,
+        y,
+        z,
+        xnew=xmeas,
+        ynew=ymeas,
+        x_label=x_label,
+        y_label=y_label,
+        z_label=cb_label,
+    )
     ax = plot_line_scoop_from_df(df, ival, ax=ax)
     return ax
 
@@ -147,27 +228,37 @@ def plot_line_scoop_from_df(df, ival, ax=None):
     ax = if_not_ax_make_ax(ax)
     df_plot = line_scoop(df, ival)
     col_names = list(df_plot.columns)
-    df_plot.plot(col_names[0], col_names[1],
-                 linestyle='--', marker='o', ax=ax)
+    df_plot.plot(col_names[0], col_names[1], linestyle="--", marker="o", ax=ax)
     return ax
 
 
 def line_scoop(df, ival):
     col_names = list(df.columns)
-    df_plot = df[(df[col_names[1]] >= ival[0]) & (df[col_names[1]]<=ival[1])].sort_values(col_names[0])
+    df_plot = df[
+        (df[col_names[1]] >= ival[0]) & (df[col_names[1]] <= ival[1])
+    ].sort_values(col_names[0])
     return df_plot[[col_names[0], col_names[2]]]
 
 
-def data_array_to_sorted_cordinate_df(x, y, z, xnew=None, ynew=None,
-                                      x_label: str = 'X',
-                                      y_label: str = 'Y',
-                                      z_label: str = 'Z'):
-    if x_label is None: x_label = 'X'
-    if y_label is None: y_label = 'Y'
-    if z_label is None: z_label = 'Z'
+def data_array_to_sorted_cordinate_df(
+    x,
+    y,
+    z,
+    xnew=None,
+    ynew=None,
+    x_label: str = "X",
+    y_label: str = "Y",
+    z_label: str = "Z",
+):
+    if x_label is None:
+        x_label = "X"
+    if y_label is None:
+        y_label = "Y"
+    if z_label is None:
+        z_label = "Z"
 
     z_data = []
-    for ix, vx in enumerate(x): 
+    for ix, vx in enumerate(x):
         for iy, vy in enumerate(y):
             if xnew is None:
                 xij = vx
@@ -178,7 +269,7 @@ def data_array_to_sorted_cordinate_df(x, y, z, xnew=None, ynew=None,
             else:
                 yij = ynew[iy][ix]
             z_data.append([xij, yij, z[iy][ix]])
-    df =pd.DataFrame(z_data, columns=[x_label, y_label, z_label])
+    df = pd.DataFrame(z_data, columns=[x_label, y_label, z_label])
     df = df.dropna()
     df_s = df.sort_values([x_label, y_label])
     return df_s
@@ -186,9 +277,9 @@ def data_array_to_sorted_cordinate_df(x, y, z, xnew=None, ynew=None,
 
 def style_jose(ax):
     ax.set_axisbelow(True)
-    ax.grid(which='major', color='black', linewidth=1.2)
-    ax.grid(which='minor', color='black', linewidth=0.6)
-    ax.set_facecolor('#708099')
+    ax.grid(which="major", color="black", linewidth=1.2)
+    ax.grid(which="minor", color="black", linewidth=0.6)
+    ax.set_facecolor("#708099")
     return ax
 
 
@@ -201,45 +292,43 @@ def if_not_ax_make_ax(ax):
 def set_real_aspect(ax):
     ylim = ax.get_ylim()
     xlim = ax.get_xlim()
-    aspect = (ylim[1]- ylim[0])/(xlim[1]- xlim[0])
+    aspect = (ylim[1] - ylim[0]) / (xlim[1] - xlim[0])
     ax.set_aspect(aspect)
     return ax
 
 
 def change_unit_axis(axes, axis, scale: float, newlabel: Optional[str] = None):
 
-    formatter = FuncFormatter(
-                              partial(unit_scale, scale=scale)
-                             )
+    formatter = FuncFormatter(partial(unit_scale, scale=scale))
 
-    if axis in ['x', 'X']:
+    if axis in ["x", "X"]:
         axes.xaxis.set_major_formatter(formatter)
         if newlabel is None:
             label = axes.get_xlabel()
-            axes.set_xlabel(f'{label}{scale}')
+            axes.set_xlabel(f"{label}{scale}")
         else:
             axes.set_xlabel(newlabel)
 
-    elif axis in ['y', 'Y']:
+    elif axis in ["y", "Y"]:
         axes.yaxis.set_major_formatter(formatter)
         if newlabel is None:
             label = axes.get_ylabel()
-            axes.set_ylabel(f'{label}{scale}')
+            axes.set_ylabel(f"{label}{scale}")
         else:
             axes.set_ylabel(newlabel)
 
-    elif axis in ['cb', 'colorbar', 'z', 'Z']:
+    elif axis in ["cb", "colorbar", "z", "Z"]:
         axes.formatter = formatter
         axes.update_ticks()
         if newlabel is None:
             label = axes.get_label()
-            axes.set_label(f'{label}{scale}')
+            axes.set_label(f"{label}{scale}")
         else:
             axes.set_label(newlabel)
 
     return axes
 
 
-def unit_scale(x, pos, scale:float):
+def unit_scale(x, pos, scale: float):
 
-    return '%1.1f' % (x/scale)
+    return "%1.1f" % (x / scale)
