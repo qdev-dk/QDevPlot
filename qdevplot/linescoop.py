3825fcb50f08d106986c00e62acd4ac0b370a93f
RasmusBC59 <rbcmail@gmail.com>
2023-05-31 16:25:12 +0200

    update fra fredrik

--- qdevplot/linescoop.py
+++ qdevplot/linescoop.py
@@ -58,6 +58,9 @@ class LineScoop:
     def ylim(self):
         return (min(self.p1[1], self.p2[1]), max(self.p1[1], self.p2[1]))
 
+    def range_col(self, col):
+        return col.max() - col.min()
+
     def plot_line_scoop_from_df_line(
         self,
         a,
@@ -65,29 +68,46 @@ class LineScoop:
         labels=None,
     ):
         self.ax_line = if_not_ax_make_ax(self.ax_line)
-        self.df_plot = get_scoop_line(
+        a_norm, self.df_plot = get_scoop_line(
             self.df, a, b, self.delta, xlim=self.xlim, ylim=self.ylim
         )
         col_names = list(self.df_plot.columns)
-        scale = self.df_plot[col_names[2]].max() - self.df_plot[col_names[2]].min()
-        dist_to_point = np.array(
-            [
-                [abs(x * int(x > 0)) * scale, abs(x * int(x <= 0) * scale)]
-                for x in self.df_plot["distsign"].values
-            ]
-        ).T
 
+        
+        dist_to_point = np.array(self.df_plot["distsign"].values)
+        # dist_to_point = np.array(
+        #     [
+        #         [abs(x * int(x > 0)) * scale, abs(x * int(x <= 0) * scale)]
+        #         for x in self.df_plot["distsign"].values
+        #     ]
+        # ).T
+        #self.df_plot[col_names[0]]
         self.df_plot.plot(
             col_names[0],
             col_names[2],
             linestyle="--",
             marker="o",
             ax=self.ax_line,
-            yerr=dist_to_point,
+            label='data',
         )
-
-        self.ax_line.legend(
-            f"t({col_names[0]} + {a:.2f}{col_names[1]}) + {b:.2f}{col_names[1]}"
+                
+        print('Points in line-scoop')
+        print('x-coordinates: ', self.df_plot[col_names[0]].tolist())
+        print('y-coordinates: ', self.df_plot[col_names[1]].tolist())
+        
+        self.ax_line.quiver(
+            self.df_plot[col_names[0]].tolist(), self.df_plot[col_names[2]].tolist(),
+            dist_to_point * np.sin(-np.arctan(a_norm)) * range_of_column(self.df_plot[col_names[0]]),
+            dist_to_point * np.cos(np.arctan(a_norm)) * range_of_column(self.df_plot[col_names[2]]),
+            color=["r"],
+            label='reference',
+            angles='xy', 
+            scale_units='xy',
+            scale=1,
+        )
+        
+        self.ax_line.legend(loc='best', fontsize=7,
+            #f"t({col_names[0]} + {a:.2f}{col_names[1]}) + {b:.2f}{col_names[1]}"
         )
         self.ax_line.set_title(
             f"t({col_names[0]} + {a:.2f}{col_names[1]}) + {b:.2f}{col_names[1]}"
@@ -107,18 +127,29 @@ def get_line_from_two_points(point_1: tuple, point_2: tuple):
     return a, b
 
 
-def get_scoop_line(df, a, b, delta, xlim=(-np.inf, np.inf), ylim=(-np.inf, np.inf)):
+def range_of_column(col):
+    return col.max() - col.min()
+
+
+def get_scoop_line(df, a, b, delta, xlim=(-np.inf, np.inf), ylim=(-np.inf, np.inf), scale=None):
     col_names = list(df.columns)
     X = col_names[0]
     Y = col_names[1]
     Z = col_names[2]
 
-    df["distsign"] = (df[X] * a + b - df[Y]) / (a**2 + 1) ** 0.5
+    if scale == None:
+        a_norm = a * range_of_column(df[col_names[0]]) / range_of_column(df[col_names[1]])
+    
+    
+    df["distsign"] = (df[X] * a + b - df[Y]) / range_of_column(df[col_names[1]]) * a_norm / (
+        a_norm**2 + 1
+    ) ** 0.5
     df["dist"] = df["distsign"].abs()
     df["line"] = df[X] * a + b
     print(f"xlim {xlim}")
     print(f"ylim {ylim}")
-    return df[
+    
+    return a_norm, df[
         (df["dist"] < delta)
         * df[X].between(*xlim, "both")
         * df[Y].between(*ylim, "both")
