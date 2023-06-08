81cbbe60fcd699e0d315d3905190c08f4b411c92
RasmusBC59 <rbcmail@gmail.com>
2023-06-08 12:55:51 +0200

    add new get_scoop_line clean version

--- qdevplot/linescoop.py
+++ qdevplot/linescoop.py
@@ -70,19 +70,10 @@ class LineScoop:
         labels=None,
     ):
         self.ax_line = if_not_ax_make_ax(self.ax_line)
-        a_norm, self.df_plot = get_scoop_line(
+        self.df_plot = get_scoop_line(
             self.df, a, b, self.delta, xlim=self.xlim, ylim=self.ylim
         )
         col_names = list(self.df_plot.columns)
-        print(type(self.df_plot))
-        # dist_to_point = np.array(self.df_plot["distsign"].values)
-        # dist_to_point = np.array(
-        #     [
-        #         [abs(x * int(x > 0)) * scale, abs(x * int(x <= 0) * scale)]
-        #         for x in self.df_plot["distsign"].values
-        #     ]
-        # ).T
-        # self.df_plot[col_names[0]]
         self.ax_line.plot(
             self.df_plot["crossing_x"].tolist(),
             self.df_plot[col_names[2]].tolist(),
@@ -90,34 +81,8 @@ class LineScoop:
             marker="o",
             label="data",
         )
-        # self.df_plot.plot(
-        #    col_names[0],
-        #    col_names[2],
-        #    linestyle="--",
-        #    marker="o",
-        #    ax=self.ax_line,
-        #    label="data",
-        # )
-        self.ax_line.figure.canvas.draw()
-        print("Points in line-scoop")
-        print("x-coordinates: ", self.df_plot[col_names[0]].tolist())
-        print("y-coordinates: ", self.df_plot[col_names[1]].tolist())
 
-        # self.ax_line.quiver(
-        #    self.df_plot[col_names[0]].tolist(),
-        #    self.df_plot[col_names[2]].tolist(),
-        #    dist_to_point
-        #    * np.sin(-np.arctan(a_norm))
-        #    * range_of_column(self.df_plot[col_names[0]]),
-        #    dist_to_point
-        #    * np.cos(np.arctan(a_norm))
-        #    * range_of_column(self.df_plot[col_names[2]]),
-        #    color=["r"],
-        #    label="reference",
-        #    angles="xy",
-        #    scale_units="xy",
-        #    scale=1,
-        # )
+        self.ax_line.figure.canvas.draw()
 
         self.ax_line.legend(
             loc="best",
@@ -146,40 +111,6 @@ def range_of_column(col):
     return col.max() - col.min()
 
 
-def get_scoop_line_old(
-    df, a, b, delta, xlim=(-np.inf, np.inf), ylim=(-np.inf, np.inf), scale=None
-):
-    col_names = list(df.columns)
-    X = col_names[0]
-    Y = col_names[1]
-    Z = col_names[2]
-
-    if scale == None:
-        a_norm = (
-            a * range_of_column(df[col_names[0]]) / range_of_column(df[col_names[1]])
-        )
-
-    df["distsign"] = (
-        (df[X] * a + b - df[Y])
-        / range_of_column(df[col_names[1]])
-        * a_norm
-        / (a_norm**2 + 1) ** 0.5
-    )
-    df["dist"] = df["distsign"].abs()
-    df["line"] = df[X] * a + b
-    print(f"xlim {xlim}")
-    print(f"ylim {ylim}")
-
-    return (
-        a_norm,
-        df[
-            (df["dist"] < delta)
-            * df[X].between(*xlim, "both")
-            * df[Y].between(*ylim, "both")
-        ].sort_values([X, Y])[[X, Y, Z, "distsign"]],
-    )
-
-
 def get_scoop_line(
     df, a, b, delta, xlim=(-np.inf, np.inf), ylim=(-np.inf, np.inf), scale=None
 ):
@@ -189,16 +120,13 @@ def get_scoop_line(
     Z = col_names[2]
 
     df = ad_dist_and_crossing(df, a, b)
-    return (
-        None,
-        df[
-            (df["dist"] < delta)
-            * df["crossing_x"].between(*xlim, "both")
-            * df["crossing_y"].between(*ylim, "both")
-        ].sort_values(["crossing_x", "crossing_y"])[
-            [X, Y, Z, "crossing_x", "crossing_y", "dist"]
-        ],
-    )
+    return df[
+        (df["dist"] < delta)
+        * df["crossing_x"].between(*xlim, "both")
+        * df["crossing_y"].between(*ylim, "both")
+    ].sort_values(["crossing_x", "crossing_y"])[
+        [X, Y, Z, "crossing_x", "crossing_y", "dist"]
+    ]
 
 
 def ad_dist_and_crossing(df, a, b):
